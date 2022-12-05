039905e912decf5c051ce8838a5ea47331f8e3a8
Masoud Rahmani <mrem71@gmail.com>
2022-12-05 16:14:43 +0330

    bug1

--- RunHomeUploader.bat
+++ RunHomeUploader.bat
@@ -7,7 +7,7 @@ echo [3] Restart Server
 echo [4] Delete Server
 echo [5] Show Detail
 
-CHOICE /C 123456 /n /T 40 /D 6 /M "(Exits after 40 second):"
+CHOICE /C 123456 /n /T 15 /D 6 /M "(Exits after 15 second):"
 
 echo %ERRORLEVEL%
 
@@ -16,41 +16,41 @@ if %ERRORLEVEL%==2 (goto StopServer)
 if %ERRORLEVEL%==3 (goto RestartServer)
 if %ERRORLEVEL%==4 (goto DeleteServer)
 if %ERRORLEVEL%==5 (goto ShowDetail)
-if %ERRORLEVEL%==6 (goto exit)
+if %ERRORLEVEL%==6 (goto NoSelection)
 
 
 
-:finally
-pause
-exit
+
 
 
 :StartServer
-npm run StartServer
+cmd /k npm run StartServer
 
 goto finally
 
 :StopServer
-npm run StopServer
+cmd /k npm run StopServer
 
 goto finally
 
 :RestartServer
-npm run RestartServer
+cmd /k npm run RestartServer
 
 goto finally
 
 :DeleteServer
-npm run DeleteServer
+cmd /k npm run DeleteServer
 
 goto finally
 
 :ShowDetail
-npm run info
+cmd /k npm run info
 
 goto finally
 
-:exit
+:NoSelection
 echo No Choice were selected. bye!
 
-goto finally
+:finally
+pause
+exit
